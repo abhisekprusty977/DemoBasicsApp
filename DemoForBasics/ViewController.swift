24da6733c3278b2ad4c28adac087d8e43830e9cd
abhisekprusty977 <abhisekprusty977@gmail.com>
2026-08-09 12:37:12 +0530

    Create Basic App

---
+++ DemoForBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoForBasics
+//
+//  Created by Abhisek Prusty on 09/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
